a7c264db35fd70e559e6a6566bd3aa020124294f
gab246 <119339137+gab246@users.noreply.github.com>
2023-03-14 16:57:38 +1100

    added required and minLength to password and username

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -29,11 +29,11 @@ export const LoginView = ({ onLoggedIn }) => {
     <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} required minLength='8'/>
       </label>
       <label>
         Password:
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} required minLength='8' />
       </label>
       <button type='submit'>Submit</button>
     </form>
