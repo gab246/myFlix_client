f26d62ecb4d561e61e4dbca2637cfe615dbd7808
gab246 <119339137+gab246@users.noreply.github.com>
2023-03-15 09:02:31 +1100

    added token authentication and localStorage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,45 +4,62 @@ import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view'
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
  
   useEffect(() => {
-    fetch('https://desolate-sierra-27780.herokuapp.com/movies')
+    if (!token) {
+      return;
+    }
+    fetch('https://desolate-sierra-27780.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
       .then((data) => {
-        const moviesFromApi = data.map((movie) => {
-          return {
-            id: movie._id,
-            title: movie.Title,
-            image: `https://via.placeholder.com/270x480.png?text`,
-            description: movie.Description,
-            genre: {
-            name: movie.Genre.Name,
-            description: movie.Genre.Description,
-            },
-            director: {
-            name: movie.Director.Name,
-            birth: movie.Director.Birth,
-            bio: movie.Director.Bio
-            }
+        console.log(data);
+  
+    const moviesFromApi = data.map((movie) => {
+      return {
+        id: movie._id,
+        title: movie.Title,
+        image: `https://via.placeholder.com/270x480.png?text`,
+        description: movie.Description,
+        genre: {
+          name: movie.Genre.Name,
+          description: movie.Genre.Description,
+          },
+        director: {
+          name: movie.Director.Name,
+          birth: movie.Director.Birth,
+          bio: movie.Director.Bio
+          }
             
           };
         });
   
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, [token]);
   
-  if(!user) {
-    return <LoginView onLoggedIn={(user) => setUser(user)} />;
+  if (!user) {
+    return (
+      <LoginView 
+        onLoggedIn={(user, token) => {
+          setUser(user); 
+          setToken(token); 
+    }} 
+    />
+    );
   }
 
   if (selectedMovie){
     return (
       <>
-    <button onClick={() => { setUser(null); }}>Logout</button>
+    <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
     <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     </>
   );
