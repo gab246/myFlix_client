d6e7cb50f1567c1e8ff4a4d98c37042b236bfcac
gab246 <119339137+gab246@users.noreply.github.com>
2023-03-14 16:53:51 +1100

    added verifying login details

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,39 @@
-export const LoginView = () => {
+import React from 'react';
+import { useState } from 'react'
+
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch('https://desolate-sierra-27780.herokuapp.com/login.json', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert('Login failed');
+      }
+    })
+  };
+
   return ( 
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type='text' />
+        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
       </label>
       <label>
         Password:
-        <input type='password' />
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
       </label>
       <button type='submit'>Submit</button>
     </form>
