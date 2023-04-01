b116a314553fe8eb4099aa0bc5196ebce40694fd
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-01 12:08:32 +1100

    updated for cleaner code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -41,23 +41,23 @@ return (
   <Form onSubmit={handleSubmit}>
     <Form.Group controlId='formUsername'>
       <Form.Label>Username: </Form.Label>
-    <Form.Control 
-      type='text'
-        value={username} 
+        <Form.Control 
+          type='text'
+          value={username} 
           onChange={(e) => setUsername(e.target.value)} 
-            required 
-              minLength= '6'
+          required 
+          minLength= '6'
           />
     </Form.Group>
 
-      <Form.Group controlId='formPassword'>
-        <Form.Label>Password: </Form.Label>
-          <Form.Control 
-            type='password'
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            minLength='6'
+    <Form.Group controlId='formPassword'>
+      <Form.Label>Password: </Form.Label>
+        <Form.Control 
+          type='password'
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          minLength='6'
           />
 
     </Form.Group>
