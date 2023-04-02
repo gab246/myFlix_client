f5aae5e47ac4a6b2b4843cdc0cd923c618060c30
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-02 14:25:47 +1000

    added link to movie.id, added view button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
-import './movie-card.scss';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card className='h-100' onClick={() => onMovieClick(movie)} >
+    <Card className='h-100' >
       <Card.Img variant='top' src={movie.image} />
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
+          <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+<Button variant='link'>view</Button>
+          </Link>
         </Card.Body>
     </Card>
   );
@@ -27,6 +31,5 @@ MovieCard.propTypes = {
       bio: PropTypes.string.isRequired,
       birth: PropTypes.string.isRequired,
     }).isRequired,
-    onMovieClick: PropTypes.func
   })
   };
