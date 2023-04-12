02495ad45b6edc1e3b607d70dfa149a0618b8c62
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-13 09:14:47 +1000

    cleaned up code, deleted unnecessary code

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card, Form, Col, Button } from "react-bootstrap";
 import { UserInfo } from './user-info/user-info';
 import { AddFavorite } from './add-favorite/add-favorite';
 import { UserDelete } from './user-delete/user-delete';
@@ -8,11 +7,10 @@ import { UserUpdate } from './user-update/user-update';
 export const ProfileView = () => {
   return (
     <>
-    <UserInfo />
-    <AddFavorite />
-    <UserUpdate />
-    <UserDelete />
+      <UserInfo />
+      <AddFavorite />
+      <UserUpdate />
+      <UserDelete />
     </>
-    
-)
+  )
 }
