b5f4b734538aa26bc65b15251b94a0c09f2d2481
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-13 09:04:21 +1000

    cleaned up code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,72 +1,75 @@
-import React from 'react';
-import { useState } from 'react'
-import { Button, Form, Card } from 'react-bootstrap';
+import React from "react";
+import { useState } from "react";
+import { Button, Form, Card } from "react-bootstrap";
 
 export const LoginView = ({ onLoggedIn }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const data = {
       Username: username,
-      Password: password 
-    }; 
+      Password: password,
+    };
 
-    fetch('https://desolate-sierra-27780.herokuapp.com/login', {
-      method: 'POST',
+    fetch("https://desolate-sierra-27780.herokuapp.com/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    }).then((response) => response.json())
-    .then((data) => {
-      console.log('login response: ', data);
-      if (data.user) {
-        localStorage.setItem('user', JSON.stringify(data.user));
-        localStorage.setItem('token', data.token);
-        onLoggedIn(data.user, data.token);
-      } else {
-        alert('This user does not exist');
-      }
+      body: JSON.stringify(data),
     })
-    .catch((e) => {
-      alert('Something went wrong');
-    });
-  }
-
-return ( 
-  <Card className='mt-5 mb-5'>
-    <Card.Body>
-      <Card.Title>LOGIN</Card.Title>
-      <Form onSubmit={handleSubmit} className='mt-4 mb-4'>
-        <Form.Group controlId='formUsername' className='mt-4 mb-4'>
-          <Form.Label>Username: </Form.Label>
-          <Form.Control 
-            type='text'
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-            required 
-            minLength= '6'
-            className='bg-light'
-          />
-    </Form.Group>
-
-        <Form.Group controlId='formPassword' className='mt-4 mb-4'>
-          <Form.Label>Password: </Form.Label>
-          <Form.Control 
-            type='password'
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            minLength='6'
-            className='bg-light'
-          />
-
-    </Form.Group>
-        <Button variant='primary' type='submit' className='mt-4'> LOGIN!</Button>
-    </Form>
-    </Card.Body>
-  </Card>
-  );
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("This user does not exist");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
   };
+
+  return (
+    <Card className="mt-5 mb-5">
+      <Card.Body>
+        <Card.Title>LOGIN</Card.Title>
+        <Form onSubmit={handleSubmit} className="mt-4 mb-4">
+          <Form.Group controlId="formUsername" className="mt-4 mb-4">
+            <Form.Label>Username: </Form.Label>
+            <Form.Control
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              minLength="6"
+              className="bg-light"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword" className="mt-4 mb-4">
+            <Form.Label>Password: </Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              minLength="6"
+              className="bg-light"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="mt-4">
+            {" "}
+            LOGIN!
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
