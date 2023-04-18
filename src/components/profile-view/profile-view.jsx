68c020df09d722810fd5d3291486fb4177b7de1f
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-18 14:53:01 +1000

    added account details and favorite movie cards

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,16 +1,40 @@
-import React from "react";
-import { UserInfo } from './user-info/user-info';
-import { AddFavorite } from './add-favorite/add-favorite';
-import { UserDelete } from './user-delete/user-delete';
-import { UserUpdate } from './user-update/user-update';
+import { useState } from "react";
+import { Card, Col, Form, Button } from "react-bootstrap";
+import { MovieCard } from "../movie-card/movie-card";
 
-export const ProfileView = () => {
-  return (
-    <>
-      <UserInfo />
-      <AddFavorite />
-      <UserUpdate />
-      <UserDelete />
-    </>
-  )
-}
+export const ProfileView = ({ user, token, movies, onLoggedOut, updateUser }) => {
+
+
+    return (
+        <>
+          <h1>Account Details</h1>
+
+          <Col md={6}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Account Details</Card.Title>
+                  <p className="mt-4 mb-4">Username: {user.Username}</p>
+                  <p className="mt-4 mb-4">Email: {user.Email}</p>
+                  <p className="mt-4 mb-4">Date of Birth: {user.Birthday.slice(0,10)}</p>
+              </Card.Body>
+            </Card>
+    
+          <Col md={12}>
+            <Card className='mt-4 mb-4'>
+              <Card.Body>
+                <Card.Title>Your Favorite Movies</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col> 
+          <Col md={6}>
+            {favoriteMovies.map(movie => (
+          <Col className="mb-4 mt-4" key={movie._id} >
+            <MovieCard movie={movie} />
+          </Col>
+        )) 
+      }
+    </Col>
+  </Col>
+            
+       
+      )
