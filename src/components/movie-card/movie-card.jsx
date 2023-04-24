c55bd3e2166a35ed51f34149575467b8e098c732
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-24 10:19:51 +1000

    imported scss

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './movie-card.scss';
 
 export const MovieCard = ({ movie }) => {
   return (
